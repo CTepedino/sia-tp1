112ae96aceccbd95a227098cca71e273703e596a
alberto <abendayan@itba.edu.ar>
2025-03-27 00:14:43 -0300

    news

--- graphicsGenerator/draw_graphics_compare_heuristics_time.py
+++ graphicsGenerator/draw_graphics_compare_heuristics_time.py
@@ -7,44 +7,48 @@ import os
 json_path = sys.argv[1]
 
 file_name = os.path.splitext(os.path.basename(json_path))[0]
-output_file = f"fgrafico_comparacion_heuristicas_manhattan_distance_sum_{file_name}.png"
+output_file = f"fgrafico_comparacion_heuristicas_walled_distance_sum_{file_name}.png"
 
 with open(json_path, "r") as f:
     data = json.load(f)
 
-heuristics = ["manhattan_distance_sum", "manhattan_distance_sum_adl"]
-heuristics2 = ["Manhattan distance", "Manhattan distance avoid deadlocks"]
+# heuristics = ["manhattan_distance_sum", "manhattan_distance_sum_adl"]
+# heuristics2 = ["Manhattan distance", "Manhattan distance avoid deadlocks"]
 # heuristics = ["nearest_box", "nearest_box_adl"]
 # heuristics2 = ["Nearest box", "Nearest box avoid deadlocks"]
-# heuristics = ["walled_distance_sum", "walled_distance_sum_adl"]
-# heuristics2 = ["Walled distance sum", "Walled distance sum avoid deadlocks"]
+heuristics = ["walled_distance_sum", "walled_distance_sum_adl"]
+heuristics2 = ["Walled distance sum", "Walled distance sum avoid deadlocks"]
 
 a_star_times = []
+a_star_std_devs = []
 
 for heuristic in heuristics:
-
-    # a_star_data = data["a_star"][heuristic]["executions"]
-
-
-    # a_star_avg_time = np.mean([float(exec_data["time"].replace("s", "")) for exec_data in a_star_data])
     a_star_avg_time = data["a_star"][heuristic]["time_mean"]
-
+    a_star_dsv = data["a_star"][heuristic]["time_std"]
 
     a_star_times.append(a_star_avg_time)
+    a_star_std_devs.append(a_star_dsv)
 
 bar_width = 0.35
 index = np.arange(len(heuristics))
 
 plt.figure(figsize=(8, 6))
 
-
-plt.bar(index + bar_width, a_star_times, bar_width, color="red")
+# Agregar barras de error con el desvío estándar
+plt.bar(
+    index + bar_width,
+    a_star_times,
+    bar_width,
+    color="red",
+    yerr=a_star_std_devs,  # Agregar barras de error
+    capsize=5,  # Tamaño de las líneas de error
+    label="Tiempo promedio ± std"
+)
 
 plt.ylabel("Tiempo promedio (segundos)")
-
 plt.title("Comparación de tiempos de A*")
-plt.xticks(index + bar_width , heuristics2)
-
+plt.xticks(index + bar_width, heuristics2)
+plt.legend()
 
 plt.tight_layout()
 plt.savefig(output_file)
